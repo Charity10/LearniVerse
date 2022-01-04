5025ce0f8261f23ac333ff41f3e18f91ae904018
Charity <umercy001@gmail.com>
2022-01-04 21:07:58 +0100

    Documentation

---
+++ frontend/src/documentation.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const documentation = () =>{
+    return (
+
+
+             <documentation> 
+             {/* <link rel="stylesheet"href="https://fonts.googleapis.com/css?family=Tangerine"> */}
+
+          <h1>Learners Connect Documentation</h1>
+          <h3 align ='left'>OVERVIEW</h3>
+             <p align = 'left'>Learner’s connect is a website which connect students
+                (anyone who needs to learn a course,skills etc) and teachers
+                ( anyone who has knowledge in a particular field). The website is a learning platform which
+                focuses not only on school activities, For example a student who needs help in a certain assignment finds another student who is knowledgeable in that area, <br/>a skill,
+                learn to cook a particular dish, need help with your project, if you want to learn something learner's connect got you😚</p>
+        <h3 align ='left'>PROBLEM</h3>
+            <p align = 'left'>A lot of people are deficient in lot of area(s),
+ Finding someone who can help in some of these areas is really difficult and sometimes when we find someone who is willing to teach their prices are very high
+ </p>
+ <h3 align ='left'>SOLUTION</h3>
+ <p align = 'left'>Learner's connect won't only find someone who is willing to teach but someone capable of teaching and classes would be at a very affordable price</p>
+ <h3 align ='left'>WHO CAN USE THE APP</h3>
+ <p align ='left'>Only students(workers) of Obafemi Awolowo University(OAU) can use the web application as users are to log in with their OAU email</p>
+ <h3 align = 'left' >HOW THE WEBSITE WORKS</h3> 
+ <p align = 'left'>
+
+Create an account 
+- Sign up and verify your account with your student email
+- Update your page with the required details
+
+
+Update your skill
+- Create a skill card that you know you are an expert in it
+- You can also search for other's 
+
+Share your skill card
+- Upload your card on timeline for other users to connect
+- Keep learning and connecting with others</p>
+ </documentation>
+
+
+
+            
+    
+                
+        
+        
+    )
+
+    }
+
+
+export default documentation;
